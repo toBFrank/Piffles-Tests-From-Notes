8ac5adaf9d46dcf2547d1ca0451f74d646ddc9a5
Franco <bonilla.franco484@gmail.com>
2023-04-13 13:31:12 -0600

    uncoupled TxtConverter from file input

--- TxtConverter.py
+++ TxtConverter.py
@@ -8,50 +8,49 @@
 import pypdf
 
 
-
 class TxtConverter:
-    def __init__(self, file):
-        self.file = file
-        self.file_type = file.split('.')[-1]
+    def __init__(self):
+        self.supported_file_types = ["pdf"]
 
-        self.supported_file_types = ['pdf']
-
-
-    def convert(self) -> None:
-        '''
+    def convert(self, filename: str) -> str:
+        """
         Extracts text from a file and returns it as a string.
-        
+
         Parameters - None
-        Returns - None
-        '''
-        if self.file_type not in self.supported_file_types:
-            raise Exception('File type not supported.')
-        
+        Returns - filename_txt: str of the name of the .txt file
+        """
+        file_type = filename.split(".")[-1]
+        if file_type not in self.supported_file_types:
+            raise ValueError(
+                f"File type not supported. Supported file types: {self.supported_file_types}"
+            )
+
         # 1.0 Get list of dirty text
-        if self.file_type == 'pdf':
-            dirty_list = self._convert_pdf()
-        elif self.file_type == 'ppt':
-            dirty_list = self._convert_ppt()
+        if file_type == "pdf":
+            dirty_list = self._convert_pdf(filename)
+        elif file_type == "ppt":
+            dirty_list = self._convert_ppt(filename)
 
         # 2.0 Write to .txt file
-        self._make_txt_file(dirty_list)
-        
+        filename_txt = self._make_txt_file(dirty_list, filename)
 
-    def _convert_pdf(self) -> list[str]:
-        '''
+        return filename_txt
+
+    def _convert_pdf(self, filename: str) -> list[str]:
+        """
         Extracts text from a pdf file and returns it as a string.
-        
+
         Parameters - None
         Returns - dirty_list: list[str]
-        '''
+        """
         dirty_list = []
-        dirty_pdf = pypdf.PdfReader(self.file)
+        dirty_pdf = pypdf.PdfReader(filename)
 
         # 1.1 Get pdf info
         pdf_info = dirty_pdf.metadata
         num_pages = len(dirty_pdf.pages)
         pdf_info_str = (
-            f"[ {self.file} ] Details\n"
+            f"| {filename} | Details\n"
             f"Author: {pdf_info.author}\n"
             f"Creator: {pdf_info.creator}\n"
             f"Producer: {pdf_info.producer}\n"
@@ -67,31 +66,31 @@ class TxtConverter:
         # print(dirty_list)
 
         return dirty_list
-    
 
-    def _convert_ppt(self) -> list[str]:
-        '''
+    def _convert_ppt(self, filename: str) -> list[str]:
+        """
         Extracts text from a ppt file and returns it as a string.
-        
+
         Parameters - None
         Returns - dirty_list: list[str]
-        '''
+        """
         raise NotImplementedError("Coming soon!")
-    
 
-    def _make_txt_file(self, dirty_list: list[str]) -> None:
-        '''
+    def _make_txt_file(self, dirty_list: list[str], filename: str) -> str:
+        """
         Writes a list of strings to a .txt file.
-        
+
         Parameters - dirty_list: list[str]
         Returns - None
-        '''
+        """
         # 1.0 Get file name
-        file_name = self.file.split('.')[0] + '.txt'
+        file_name = filename[:-4] + ".txt"
 
         # 2.0 Write to .txt file
-        with open(file_name, 'w', encoding="utf-8") as f:
+        with open(file_name, "w", encoding="utf-8") as f:
             for item in dirty_list:
                 f.write(item)
-                f.write('\n')
-        print(f'File saved as {file_name}.')
+                f.write("\n")
+        print(f"File saved as {file_name}.")
+
+        return file_name
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 ################################################################################
-# Title: Piffles - TxtConverter main
+# Title: Piffles - TxtConverter Question maker
 # Description: Converts raw files (.pdf) to text files.
 # Author: Franco Bonilla
 # Date: 2023-04-11
@@ -17,11 +17,14 @@ def main() -> None:
     Returns - None
     '''
     # 1.0 Get file name
-    file = input('Enter file name: ')
+    filename = input('Enter file name: ')
     
     # 2.0 Convert file
-    converter = TxtConverter(file)
-    converter.convert()
+    converter = TxtConverter()
+    filename_txt = converter.convert(filename)
+
+    # 3.0 Get key words/concepts
+    # natLangProccessor = NatLangProcessor(self.file.split('.')[0] + '.txt')
     
 
 if __name__ == '__main__':
